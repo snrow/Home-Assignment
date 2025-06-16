2af5cf03a58374b27a4a8f4ac534e50229525c2c
Eliran Ben Maor <eliranbenmaor@gmail.com>
2025-06-16 14:41:37 +0300

    testing qu service piepline

--- microservices/queue-worker-service/app.py
+++ microservices/queue-worker-service/app.py
@@ -4,7 +4,7 @@ import time
 import json
 import uuid
 import logging
-  
+   
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
